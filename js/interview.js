90023487adbbf078f6c8541aee8e49d6b2c7a320
Maxim <dasvero@gmail.com>
2021-08-16 17:40:58 +0300

    added loading animations and request pop-up

--- js/interview.js
+++ js/interview.js
@@ -3,6 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const interviewBlock = document.querySelector('.interview__block');
 
+    function scroll() {
+        window.scrollTo = 250;
+    }
 
     function show() {
         setTimeout(() => {
@@ -19,6 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         setTimeout(() => {
             interviewBlock.innerHTML = block;
+            scroll();
             show();
         }, 1000);
     }
@@ -148,6 +152,9 @@ document.addEventListener('DOMContentLoaded', () => {
                 selectList.forEach(selectItem => {
                     selectItem.addEventListener('click', (event) => {
                         selectBlock.querySelector('.select_styled').textContent = selectItem.textContent;
+                        if (selectBlock.classList.contains('month')) {
+                            dateOfBirth['month'] = +selectItem.value;
+                        }
                         hideSelect(selectBlock);
                     });
                 });
@@ -155,8 +162,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
             button.addEventListener('click', () => {
                 let error = 0;
-                selects.forEach(select => {
-                    const selectStyled = select.querySelector('.select_styled');
+                selects.forEach(selectBlock => {
+                    const selectStyled = selectBlock.querySelector('.select_styled');
                     if (selectStyled.textContent == 'День' || selectStyled.textContent == 'Месяц' || selectStyled.textContent == 'Год') {
                         selectStyled.classList.add('required');
                         error = 1;
@@ -164,6 +171,15 @@ document.addEventListener('DOMContentLoaded', () => {
                 });
 
                 if (error == 0) {
+                    selects.forEach(selectBlock => {
+                        const selectStyled = selectBlock.querySelector('.select_styled');
+                        if (selectBlock.classList.contains('day')) {
+                            dateOfBirth['day'] = +selectStyled.textContent;
+                        } else if (selectBlock.classList.contains('year')) {
+                            dateOfBirth['year'] = +selectStyled.textContent;
+                        }
+                    });
+
                     loadLoadSpinner();
                 }
             });
@@ -232,10 +248,31 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function loadQuestion5() {
-        let question5 = `<div class="interview__subtitle">
-                <p class="subtitle">
-                    Смерть родного человека – одно из тяжелейших испытаний в жизни каждого из нас!
-                </p>
+        const yearsOld = Math.floor((Date.parse(new Date()) - 
+            Date.parse(`${dateOfBirth.year}-${dateOfBirth.month}-${dateOfBirth.day}`))/(1000*60*60*24*365));
+
+        let text;
+
+        if (yearsOld >= 18 && yearsOld <= 35) {
+            text = `<div class="interview__message question5">
+                    По вам скучает очень близкий человек, 
+                    которого больше нет в мире живых.
+                </div>`;
+        } else if (yearsOld >= 36 && yearsOld <= 45) {
+            text = `<div class="interview__message question5">
+                    По вам скучает очень близкий человек, 
+                    которого больше нет в мире живых. <br>Возможно это дедушка или бабушка.
+                </div>`;
+        } else if (yearsOld >= 46) {
+            text = `<div class="interview__message question5">
+                    По вам скучает очень близкий человек, 
+                    которого больше нет в мире живых. <br>Возможно это кто-то из Ваших родителей.
+                </div>`;
+        }
+
+
+        let question5 = `<div class="interview__subtitle question5">
+                
             </div>
             <div class="divider"></div>
             
@@ -251,24 +288,83 @@ document.addEventListener('DOMContentLoaded', () => {
             <p class="question__num">Вопрос 5-5</p>
             
             <img src="./img/eye.svg" alt="eye1" class="interview__eye1">
-            <img src="./img/eye-1.svg" alt="eye2" class="interview__eye2">`;
+            <img src="./img/eye-1.svg" alt="eye2" class="interview__eye2"> -->`;
 
         innerBlock(question5);
+
+
+        setTimeout(() => {
+            document.querySelector('.interview__subtitle').innerHTML = text;
+            setTimeout(() => {
+                document.querySelector('.interview__message').style.opacity = 1;
+            },1000);
+        }, 1100);
 
         setTimeout(() => {
             interviewBlock.querySelectorAll('button').forEach(button => {
                 button.addEventListener('click', () => {
-                    loadFinal();
+                    loadRecord();
                 });
             });
-        }, 1100);
+        }, 1200);
     }
 
+    function loadRecord() {
+        const record = `<div class="loading__wrapper">
+                <div class="loading__block">
+                    <img src="./img/microphone.svg" alt="mircophone" class="microphone_img">
+
+                    <div class="status__bar">
+                        <div class="status__bar__ready"></div>
+                    </div>
+                    
+                    <h1 class="status__percent">0%</h1>
+                    <div class="loading__title status__title">
+                        Запись сообщения
+                    </div>
+                </div>
+            </div>`;
+        
+        let statusValue = 0; 
+
+        innerBlock(record);
+
+        let statusBar, statusPercent, timer; 
+
+        setTimeout(() => {
+            statusBar = document.querySelector('.status__bar .status__bar__ready');
+            statusPercent = document.querySelector('.status__percent');
+        }, 1100);
+
+        setTimeout(() => {
+            timer = setInterval(loadAnimation, 200);
+        }, 1300);
+        
+
+        function loadAnimation() {
+            statusValue += Math.floor(Math.random() * (10 - 1)) + 1;
+            statusBar.style.width = `${statusValue}%`;
+            statusPercent.textContent = `${statusValue}%`;
+            if (statusValue >= 100) {
+                statusBar.style.width = `100%`;
+                statusPercent.textContent = `100%`;
+                setTimeout(loadFinal, 500);
+                clearInterval(timer);
+            }
+        }
+        
+    }
+
+
     function loadFinal() {
-        const date = new Date(Date.parse(new Date()) + 1000 * 60 * 60 * 24),
+        let date = new Date(Date.parse(new Date()) + 1000 * 60 * 60 * 24),
               day = date.getDate(),
               month = date.getMonth() + 1,
               year = date.getFullYear();
+
+              if (month < 10) {
+                  month = `0${month}` ;
+              }
 
         interviewBlock.classList.add('final_block');
         
@@ -282,7 +378,7 @@ document.addEventListener('DOMContentLoaded', () => {
             
             <div class="interview__final_block">
                 <h1 class="question__title final_title">
-                    <span> Первое значимое событие может произойти уже ${day + 1}.${month}.${year}</span>, Вам надо быть готовым, чтобы последствия не оказались необратимыми.
+                    <span> Первое значимое событие может произойти уже ${day}.${month}.${year}</span>, Вам надо быть готовым, чтобы последствия не оказались необратимыми.
             
                 </h1>
             </div>
@@ -298,6 +394,64 @@ document.addEventListener('DOMContentLoaded', () => {
             </p>`;
 
         innerBlock(finalQuestion);
+
+        setTimeout(() => {
+            const btn = document.querySelector('button');
+
+            btn.addEventListener('click', () => {
+                const request = new XMLHttpRequest();
+
+                request.open('GET', 'https://swapi.dev/api/people/1/');
+
+                request.setRequestHeader('Content-type', 'application/json;');
+
+                request.send();
+    
+                request.addEventListener('load', () => {
+                    if (request.status === 200) {
+                        const data = JSON.parse(request.response);
+
+                        console.log(data);
+                        
+                        const requestHTML = `<div class="request__wrapper">
+                                <div class="request__block">
+                                    <h3 class="request__text">Имя: ${data.name}</h3>
+                                    <h3 class="request__text">Рост: ${data.height}</h3>
+                                    <h3 class="request__text">Вес: ${data.masss}</h3>
+                                    <h3 class="request__text">Цвет волос: ${data.hair_color}</h3>
+                                    <h3 class="request__text">Цвет кожи: ${data.skin_color}</h3>
+                                    <h3 class="request__text">Цвет глаз: ${data.eye_color}</h3>
+                                    <h3 class="request__text">Год рождения: ${data.birth_year}</h3>
+                                    <h3 class="request__text">Пол: ${data.gender}</h3>
+                                    <h3 class="request__text">Место рождения: <a href="${data.homeworld}">${data.homeworld}</a></h3>
+                                    <h3 class="request__text">Фильмы: <a href="${data.films[0]}">1</a> <a href="${data.films[1]}">2</a> 
+                                        <a href="${data.films[2]}">3</a> <a href="${data.films[3]}">6</a></h3>
+                                    <h3 class="request__text">Вид: ${data.species}</h3>
+                                    <h3 class="request__text">Транспорт: <a href="${data.vehicles[0]}">1</a> <a href="${data.vehicles[1]}">2</a></h3>
+                                    <h3 class="request__text">Корабли: <a href="${data.starships[0]}">1</a> <a href="${data.starships[1]}">2</a></h3>
+                                    <h3 class="request__text">Создано: ${new Date(data.created).getDate()}.${new Date(data.created).getMonth() + 1}.${new Date(data.created).getFullYear()}</h3>
+                                    <h3 class="request__text">Изменено: ${new Date(data.edited).getDate()}.${new Date(data.edited).getMonth() + 1}.${new Date(data.edited).getFullYear()}</h3>
+                                    <h3 class="request__text">Url: ${data.url}</h3>
+                                </div>
+                            </div>`;
+
+                            interviewBlock.innerHTML += requestHTML;
+
+                            setTimeout(() => {
+                                const requestBlock = document.querySelector('.request__wrapper');
+
+                                requestBlock.addEventListener('click', (event) => {
+                                    if (event.target && event.target.classList.contains('request__wrapper')) {
+                                        requestBlock.remove();
+                                    }
+                                });
+                            },100);
+                    } else {
+                        console.log('Что-то пошло не так');
+                    }
+                });
+            });
+        }, 1100);
     }
 
 
